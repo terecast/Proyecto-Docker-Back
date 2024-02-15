1ca70aa91f2eecf89cb54c6dce97b7dd862ca3d2
tere <125310682+terecast@users.noreply.github.com>
2024-02-15 11:31:20 -0600

    var 2

--- server.js
+++ server.js
@@ -10,12 +10,9 @@ connectDb()
 const app = express()
 app.use(cors())
 app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "React app URL"
+    res.setHeader("Access-Control-Allow-Origin",'http://localhost:5173'
     );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    
     next();
   });
 
