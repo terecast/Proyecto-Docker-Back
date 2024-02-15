7f488cb41a699c5d2aa1d4a38478fff54e51c04f
tere <125310682+terecast@users.noreply.github.com>
2024-02-15 11:05:16 -0600

    allows

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const cors = require('cors')
 connectDb()
 const app = express()
 app.use(cors())
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
